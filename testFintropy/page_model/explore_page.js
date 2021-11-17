14ff2ccf76985222f974d586afe0365d97f81798
ajkornej <74785473+ajkornej@users.noreply.github.com>
2021-11-17 09:58:14 +0300

    Update explore_page.js

--- testFintropy/page_model/explore_page.js
+++ testFintropy/page_model/explore_page.js
@@ -1,1 +1,13 @@
-/
+import {Selector} from 'testcafe'
+
+class ExplorePage {
+   constructor(){
+      this.CreateBtn = Selector('');
+      this.ProfileBtn = Selector('');
+      this.SearchToken = Selector('');
+      this.sorting = Selector('');
+      this.tokenBtn = Selector('');
+
+   }
+}
+export default ExplorePage 
