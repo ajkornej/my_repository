31871601de49920e65584b9d1320814c6bfe3a39
ajkornej <74785473+ajkornej@users.noreply.github.com>
2021-11-17 10:42:24 +0300

    Add files via upload

---
+++ testFintropy/page_model/token_page.js
@@ -0,0 +1,15 @@
+import {Selector} from 'testcafe'
+
+class tokenPage {
+   constructor(){
+      this.CreateBtn = Selector('');
+
+      this.ExploreBtn = Selector('#Explore-button');
+     
+      this.tokenBtn = Selector('#Create-button');
+
+      
+
+   }
+}
+export default tokenPage
